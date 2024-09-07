52c5a11e3fc17be3ca90e09179892d468cb391ec
Venkata <venkataraju.bandaru8@gmail.com>
2024-09-07 18:19:30 +0530

    backend script added

--- backend.sh
+++ backend.sh
@@ -55,4 +55,36 @@ else
     echo -e "expense user exists.. $Y SKIPPING $N"
 fi
 
+mkdir -p /app &>>$LOG_FILE
+VALIDATE $? "Creating /app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading backend code"
+
+cd /app
+rm -rf /app/*
+unzip /tmp/backend.zip &>>$LOG_FILE
+VALIDATE $? "extracting back code"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "installing dependence"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+VALIDATE $? "Copying the backend code"
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Mysql claint installing"
+
+mysql -h 172.31.28.10 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+VALIDATE $? "Schema loading"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "deamon reloading"
+
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "enablening backed"
+
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "restating backend"
+
 
